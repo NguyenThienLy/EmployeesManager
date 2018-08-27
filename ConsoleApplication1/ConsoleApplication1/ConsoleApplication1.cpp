64527ebb8f922c64f824e582383596258541d589
LÝ NGUYỄN THIÊN <1612365@student.hcmus.edu.vn>
2018-08-27 15:05:57 +0700

    new

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Linh o do" << endl;
+
+    return 0;
+}
+
